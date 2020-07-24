8d532a762ccb17a3f8d655a3f679355deee6a3ed
Joshua Martinez <Joshuamartinez8723@gmail.com>
2020-07-24 14:01:20 -0600

    Primer commit

---
+++ P3Lab1_JoshuaMartinez.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main(){
+    int respuesta;
+    while(respuesta!=3){
+        cout<<"1. Ejercicio"<<endl;
+        cout<<"2. Suma"<<endl;
+        cout<<"3. Salir"<<endl;
+        cout<<"Ingrese opcion: ";
+        cin>>respuesta;
+        switch (respuesta)
+        {
+        case 1:
+            
+            break;
+        
+        case 2:
+            
+            break;
+        }  
+    }
+
+}

a5fc6c338dcf656ee7089a350e171f410f80e0ef
Joshua Martinez <Joshuamartinez8723@gmail.com>
2020-07-24 15:14:05 -0600

    Termine Ejercicio 1

--- P3Lab1_JoshuaMartinez.cpp
+++ P3Lab1_JoshuaMartinez.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main(){
     int respuesta;
     while(respuesta!=3){
-        cout<<"1. Ejercicio"<<endl;
+        cout<<"1. funcion sigmoide"<<endl;
         cout<<"2. Suma"<<endl;
         cout<<"3. Salir"<<endl;
         cout<<"Ingrese opcion: ";
@@ -12,25 +12,33 @@ int main(){
         switch (respuesta)
         {
         case 1:
-            
+        {
+            int x;
+            double euler = 2.71828;
+            double  respuesta_euler, denominador, respuesta_t;
+            cout<<"Ingrese x:";
+            cin>>x;
+            respuesta_euler = pow(euler, -x);
+            denominador = 1 + respuesta_euler;
+            respuesta_t = 1 / denominador;
+            cout<<endl;
+            cout<<"La respuesta es: "<<respuesta_t << endl<<endl;
             break;
+        }
         
         case 2:
-            int limite=0, acumulador=0;
+        {
+            int limite, acumulador=0;
             cout<<"Ingrese el limite: ";
             cin>>limite;
             for (int i = 0; i <=limite; i++)
             {
-
                 acumulador+=((2*i)-1)*((2*i)+1);
             }
             cout<<endl;
-            cout<<"La respuesta es: "<< acumulador <<endl;
+            cout<<"La respuesta es: "<< acumulador <<endl<<endl;
             break;
-
-            case 3:
-            return 0;
-            break;
+        }
         }  
     }
 
